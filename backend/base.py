20d480d1969b8e0dee7c228b841288c2949ec29b
VijayaSai Somasundaram <vsai23@VijayaSais-MacBook-Pro-2.local>
2023-01-28 23:07:34 -0500

    without last api

--- backend/base.py
+++ backend/base.py
@@ -1,12 +1,38 @@
-from flask import Flask
+import dataclasses
+from flask import Flask, Response
+from flask import request
+import os
+import openai  
+import json 
 
 api = Flask(__name__)
 
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
+@api.route('/chatgpt', methods=['POST'])
+def getcorrection():
+    # get data from body and store in data vairable
+    data = request.get_json()
+    
+    bullet = data['bullet']
+    
+    checked = data['checked']
+    instructions = "Add the following words: "
+    for word in checked:
+        instructions += word + ", "
+        
+    openai.api_key = "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"
+    res = openai.Edit.create(
+      model="text-davinci-edit-001",
+      input=bullet,
+      instruction=instructions
+    )
+        
+    resp = Response()
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    resp.headers['Content-Type'] = 'application/json'
+    resp.data = json.dumps(res)
+    return resp
 
-    return response_body
+   
+    
--- src/App.js
+++ src/App.js
@@ -2,35 +2,125 @@ import { useState } from 'react'
 import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
+import { Form, Button, Input } from 'react-bootstrap';
 
 function App() {
+  const [ list, setList ] = useState(["C++", "Python", "Agile"]);
+  const [link, setLink] = useState("");
+  const [response, setResponse] = useState("");
 
-   // new line start
-  const [profileData, setProfileData] = useState(null)
+  const updateLink = e => {
+    e.preventDefault()
+    const data = e.target.elements
+    const link = data.link.value
+    setLink(link)
+  }
 
-  function getData() {
+  const updateKeywords = e => {
+    e.preventDefault()
+    const data = e.target.elements
+    const keywords = data.keywords.value
+    const list = keywords.split(",")
+    setList(list)
+  }
+
+  const api = e => {
+    e.preventDefault()
+    const data = e.target.elements  
+    const bullet = data.BulletPoint.value
+
+    // iterate through the length of list and check if the checkbox is checked
+    // if it is checked, add it to the array
+    const checked = []
+    for (let i = 0; i < list.length; i++) {
+      if (data[list[i]].checked) {
+        checked.push(list[i])
+      }
+    }
+    console.log(bullet)
+    console.log(checked)
     axios({
-      method: "GET",
-      url:"/profile",
+      method: "POST",
+      url:"chatgpt",
+      data: {
+        bullet: bullet,
+        checked: checked
+      }
     })
     .then((response) => {
-      const res =response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
+      const res = response.data.choices[0].text
+      setResponse(res)
     }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        }
-    })}
-    //end of new line 
+      console.log(error)
+    })
+  }
+
+  const CheckboxComponent = ({ list }) => {
+    return (
+    <Form onSubmit={api}>
+      <Form.Group className="mb-3" controlId="BulletPoint">
+        <Form.Label>Bullet from Resume</Form.Label>
+        <Form.Control type="text" placeholder="Enter bullet point to be fixed" />
+      </Form.Group>
+      {list.map((item) => (
+        <Form.Group className="mb-3" controlId={item}>
+          <Form.Check type="checkbox" label={item} />
+        </Form.Group>
+      ))}
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    );
+  };
+
+  const ChatGptResponse = ({ response }) => {
+    if (response == "") {
+      return <div></div>
+    }
+    else {
+      return (
+        <div>
+          <h3>Response from ChatGPT</h3>
+          <p>{response}</p>
+        </div>
+    )
+    }
+  }
 
   return (
-    <div className="App">
-      <iframe width="1000" height="1000" src="https://docs.google.com/document/d/1wEz8OUIIiAvKYTid2TvC6kdXydKqcE5GbsVBYV6SlU0"></iframe>
-    </div>
+    <div>
+      <div>
+      <h3>Resume Link</h3>
+        <Form onSubmit={updateLink}>
+          <Form.Group className="mb-3" controlId="link">
+            <Form.Label>Link to Resume</Form.Label>
+            <Form.Control type="text" placeholder="Enter link to resume" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <h3>Keywords List</h3>
+        <Form onSubmit={updateKeywords}>
+          <Form.Group className="mb-3" controlId="keywords">
+            <Form.Label>Comma-Seperate Keywords List</Form.Label>
+            <Form.Control type="text" placeholder="Enter a comma seperate list of keywords" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+      <h3> Resume </h3>
+      <div className="App">
+        <iframe width="1500" height="600" src={link} ></iframe>
+      </div>
+      <h3> Query for better alternatives below! </h3>
+      {CheckboxComponent({ list })}
+      {ChatGptResponse({ response })}
+  </div>
+
   );
 }
 
